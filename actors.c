d0c4eb02624edf212cd2a17e92eccd7fcd3e7262
David Chiu <david.t.chiu@gmail.com>
2024-03-22 18:34:12 +0000

    added thread IDs

--- actors.c
+++ actors.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
+#include <pthread.h>
 #include <fcntl.h>
 #include "actors.h"
 
 void init_semaphores() {
     sem_unlink("/student_outside");
-    sem_unlink("/david_available");
+    sem_unlink("/adam_available");
     sem_unlink("/question_asked");
     sem_unlink("/student_got_resp");
     sem_unlink("/student_leaving");
     student_outside = sem_open("/student_outside", O_CREAT, 0600, 0);
-    david_available = sem_open("/david_available", O_CREAT, 0600, 0);
+    adam_available = sem_open("/adam_available", O_CREAT, 0600, 0);
     question_asked = sem_open("/question_asked", O_CREAT, 0600, 0);
     student_got_resp = sem_open("/student_got_resp", O_CREAT, 0600, 0);
     student_leaving = sem_open("/student_leaving", O_CREAT, 0600, 0);
 }
 
-void* david_func(void* args) {
+void* adam_func(void* args) {
     int i = 0;
     while (i < NUM_STUDENTS) {
 	   // look busy
-        printf("David: looks busy before students arrive.\n");
+        printf("Adam looks busy before students arrive.\n");
 
         sem_wait(student_outside);
 
     	// panic, student has arrived
-        printf("David is panicking!!\n");
+        printf("Adam is panicking!! \"OMG OMG OMG OMG OMG\"\n");
 
-        sem_post(david_available);
+        sem_post(adam_available);
         sem_wait(question_asked);
 
     	// yell
-        printf("David: \"OMG. Get back to work now!!\"\n");
+        printf("Adam: \"OMG. Get back to work now!!\"\n");
 
         sem_post(student_got_resp);
         sem_wait(student_leaving);
 
     	// good bye
-        printf("David: waves goodbye!\n");
+        printf("Adam waves goodbye!\n");
 	   i++;
     }
     return NULL;
@@ -46,20 +47,23 @@ void* david_func(void* args) {
 
 void* student_func(void* args) {
     sem_post(student_outside);
-    sem_wait(david_available);
+    printf("Student %ld arrives at office!\n", pthread_self());
 
     // ask question
-    printf("Student: \"Hey David, do you like bananas or oranges?\"\n");
+    sem_wait(adam_available);
+
+    // ask question
+    printf("Student %ld: \"Hey Adam, do you like bananas or oranges?\"\n", pthread_self());
 
     sem_post(question_asked);
     sem_wait(student_got_resp);
 
     // acknowledge response
-    printf("Student: \"Got it! Thanks for your help!\"\n");
+    printf("Student %ld: \"Thanks for your help!\"\n", pthread_self());
 
     sem_post(student_leaving);
 
     // bye
-    printf("Student: \"Goodbye!\"\n");
+    printf("Student %ld: \"Goodbye!\"\n", pthread_self());
     return NULL;
 }
--- main.c
+++ main.c
@@ -7,19 +7,27 @@
 
 /* globals */
 sem_t* student_outside;
-sem_t* david_available;
+sem_t* adam_available;
 sem_t* question_asked;
 sem_t* student_got_resp;
 sem_t* student_leaving;
 
+int NUM_STUDENTS;
+
 int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("usage: %s <num students>\n", argv[0]);
+		exit(0);
+	}
+	NUM_STUDENTS = atoi(argv[1]);
+
 	init_semaphores();
 
 	//create professor and student threads
-	pthread_t *david = (pthread_t*) malloc(sizeof(pthread_t));
+	pthread_t *adam = (pthread_t*) malloc(sizeof(pthread_t));
 	pthread_t *students = (pthread_t*) malloc(NUM_STUDENTS * sizeof(pthread_t));
 
-	pthread_create(david, NULL, david_func, NULL);
+	pthread_create(adam, NULL, adam_func, NULL);
 	for (int i = 0; i < NUM_STUDENTS; i++) {
 		pthread_create(&students[i], NULL, student_func, NULL);
 	}
@@ -27,24 +35,24 @@ int main(int argc, char *argv[]) {
 	for (int i = 0; i < NUM_STUDENTS; i++) {
 		pthread_join(students[i], NULL);
 	}
-	pthread_join(*david, NULL);
+	pthread_join(*adam, NULL);
 
 	//close semaphores
 	sem_close(student_outside);
-	sem_close(david_available);
+	sem_close(adam_available);
 	sem_close(question_asked);
 	sem_close(student_got_resp);
 	sem_close(student_leaving);
 
 	//delete semaphores
 	sem_unlink("/student_outside");
-	sem_unlink("/david_available");
+	sem_unlink("/adam_available");
 	sem_unlink("/question_asked");
 	sem_unlink("/student_got_resp");
 	sem_unlink("/student_leaving");
 
-	free(david);
-	david = NULL;
+	free(adam);
+	adam = NULL;
 
 	free(students);
 	students = NULL;
